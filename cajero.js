e82f032e92710f6148ed0fe2a60d14f66702d4ca
ndev <napodevesa@gmail.com>
2020-08-06 12:42:05 +0200

    function entregarDinero

--- cajero.js
+++ cajero.js
@@ -1,1 +1,40 @@
-alert ("funciona");
+class Billete
+{
+
+	constructor (v,c)
+	{
+	this.valor = v;
+	this.cantidad = c;
+	}
+
+	
+}
+
+function entregarDinero()
+{
+ 
+ 	for (var bi of caja)
+ 	{
+
+ 		console.log (bi);
+ 	}
+
+}
+
+var caja = [];
+var entregado = [];
+
+caja.push(new Billete(50,3));
+caja.push(new Billete(20,2));
+caja.push(new Billete(10,2));
+
+var dinero = 210;
+
+var div = 0;
+var papeles = 0;
+
+var b = document.getElementById("extraer");
+b.addEventListener ("click", entregarDinero);
+
+
+
